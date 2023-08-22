a518f564552b263bdced9a32bb77747cecdbe07f
Jessie Shockey <shockeyjessie92@gmail.com>
2023-08-21 22:22:06 -0600

    updated questions array for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of the project?","What is the description of this project?","What should be in the table of contents?", "What needs to be installed for this application?","How should this application be used?","What license does this project need?","What is a test for the application?","Are contributors welcome on this project?","What is your GitHub username?","What is your email address?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
