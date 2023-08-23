68a3760f891fda647f1f41d6d6e585aabbeea5c5
Jessie Shockey <shockeyjessie92@gmail.com>
2023-08-23 17:23:28 -0600

    added API's for license list

--- Develop/index.js
+++ Develop/index.js
@@ -1,78 +1,101 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const prompt = require('prompt-sync')();
 const inquirer = require("inquirer");
 const fs = require('fs');
-const markDown = require('./utils/generateMarkdown');
+//Module needed for this application
+const markDown = require('./utils/generateMarkdown.js');
 
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = ["What is the title of the project?","What is the description of this project?","What should be in the table of contents?", "What needs to be installed for this application?","How should this application be used?","What license does this project need?","What is a test for the application?","How can other developers contribute to this application?","What is your GitHub username?","What is your email address?"];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
-    fs.appendFile('README.md', markDown(data), (err) =>
-  err ? console.error(err) : console.log('Commit logged!')
-);
+    fs.appendFile('README.md', markDown.generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log('Commit logged!')
+    );
 }
 
-// TODO: Create a function to initialize app
-function init() {
-inquirer.prompt([
-{
-    type: 'input',
-    message: questions[0],
-    name: 'title',
-},
-{
-    type: 'input',
-    message: questions[1],
-    name: 'desc',
-},
-{
-    type: 'input',
-    message: questions[2],
-    name: 'contents',
-},
-{
-    type: 'input',
-    message: questions[3],
-    name: 'installs',
-},
-{
-    type: 'input',
-    message: questions[4],
-    name: 'usage',
-},
-{ //LICENSE INFO WILL NEED TO BE CHANGED HERE
-    type: 'input',
-    message: questions[5],
-    name: 'licenses',
-},
-{
-    type: 'input',
-    message: questions[6],
-    name: 'test',
-},
-{
-    type: 'input',
-    message: questions[7],
-    name: 'contribute',
-},
-{
-    type: 'input',
-    message: questions[8],
-    name: 'username',
-},
-{
-    type: 'input',
-    message: questions[9],
-    name: 'email',
-},
+//Function to start the command line prompts
+function startPrompts(licArray) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'desc',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'contents',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'installs',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'usage',
+        },
+        { //LICENSE INFO WILL NEED TO BE CHANGED HERE
+            type: 'list',
+            message: questions[5],
+            name: 'licenses',
+            choices: licArray,
+        },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'contribute',
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: questions[9],
+            name: 'email',
+        },
+        
+        ])
+        .then((response)=>{
+            writeToFile(response.title,response);
+        });
+}
 
-])
-.then((response)=>{
-    writeToFile(response.title,response)
-});
+//API function to pull most common Liscense Types
+function commonLicenses() {
+    fetch('https://api.github.com/licenses')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        const licArray = [];
+        for (let i=0; i<data.length; i++){
+            licArray.push(data[i].spdx_id);
+        }
+    startPrompts(licArray);
+    })
+}
+
+
+// Function to initialize app
+function init() {
+    commonLicenses();
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,17 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenseName) {
+  //API for badge making goes here
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
+function renderLicenseLink(license) {
+  fetch('https://api.github.com/licenses/'+license)
+  .then(function (response) {
+      return response.json();
+    })
+  .then(function (data) {
+      return data.html_url;
+  })
+}
+
+//API function for getting the license info
+ function getLicenseKey (license){
+  fetch('https://api.github.com/licenses')
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (data) {
+   const licInfo = data.filter((element)=>{
+      if (element.spdx_id===license){
+          return true
+      }
+    })
+    //renderLicenseBadge(licInfo[0].spdx_id)
+   return renderLicenseLink(licInfo.key);
+    
+  })
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+async function generateMarkdown(data) {
+ const licURL = getLicenseKey(data.licenses);
+
   return `# ${data.title}\n
   ## Description\n
   ${data.desc}\n
@@ -22,7 +52,7 @@ function generateMarkdown(data) {
   ## Usage\n
   ${data.usage}\n
   ## License\n
-  ${data.licenses}\n
+  ${licURL}\n
   ## Tests\n
   ${data.test}\n
   ## How to Contribute\n
@@ -33,4 +63,8 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown,
+  renderLicenseLink,
+  getLicenseKey,
+};
